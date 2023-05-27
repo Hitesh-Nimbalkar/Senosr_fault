da647cecf1951ead008f44af22957271ae07fa9d
Hitesh <nimbalkarhitesh@gmail.com>
2023-05-27 23:16:25 +0530

    Updated

--- sensor/components/model_trainer.py
+++ sensor/components/model_trainer.py
@@ -27,9 +27,8 @@ class ModelTrainer:
         try:
             # Define the parameter grid for hyperparameter tuning
             param_grid = {
-                'learning_rate': [0.1,0.2,0.3],
-                'max_depth': [3,5,12],
-                'min_child_weight': [1, 2, 3]
+                'learning_rate': [0.1,0.3],
+                'max_depth': [5,10,12]
             }
             
             # Create an XGBoost classifier
@@ -70,12 +69,12 @@ class ModelTrainer:
             logging.error("An error occurred during model training.")
             raise ApplicationException(e,sys)
     
-    def initiate_model_trainer(self)->ModelTrainerArtifact:
+    def initiate_model_trainer(self) -> ModelTrainerArtifact:
         try:
             train_file_path = self.data_transformation_artifact.transformed_train_file_path
             test_file_path = self.data_transformation_artifact.transformed_test_file_path
 
-            #loading training array and testing array
+            # Loading training array and testing array
             train_arr = load_numpy_array_data(train_file_path)
             test_arr = load_numpy_array_data(test_file_path)
 
@@ -85,40 +84,39 @@ class ModelTrainer:
                 test_arr[:, :-1],
                 test_arr[:, -1],
             )
-            
+
             best_params = self.perform_hyperparameter_tuning(x_train, y_train)
             model = self.train_model(x_train, y_train, best_params)
-            y_train_pred = model.predict(x_test)
+            y_train_pred = model.predict(x_train)  # Predict on the training set
 
-            
-            classification_train_metric =  get_classification_score(y_true=y_train, y_pred=y_train_pred)
-            
-            if classification_train_metric.f1_score<=self.model_trainer_config.expected_accuracy:
-                raise Exception("Trained model is not good to provide expected accuracy")
-            
-            y_test_pred = model.predict(x_test)
+            classification_train_metric = get_classification_score(y_true=y_train, y_pred=y_train_pred)
+
+            if classification_train_metric.f1_score <= self.model_trainer_config.expected_accuracy:
+                raise Exception("Trained model is not good enough to provide the expected accuracy")
+
+            y_test_pred = model.predict(x_test)  # Predict on the test set
             classification_test_metric = get_classification_score(y_true=y_test, y_pred=y_test_pred)
 
+            # Overfitting and Underfitting
+            diff = abs(classification_train_metric.f1_score - classification_test_metric.f1_score)
 
-            #Overfitting and Underfitting
-            diff = abs(classification_train_metric.f1_score-classification_test_metric.f1_score)
-            
-            if diff>self.model_trainer_config.overfitting_underfitting_threshold:
-                raise Exception("Model is not good try to do more experimentation.")
+            if diff > self.model_trainer_config.overfitting_underfitting_threshold:
+                raise Exception("Model is not good, try to do more experimentation.")
 
             preprocessor = load_object(file_path=self.data_transformation_artifact.transformed_object_file_path)
-            
+
             model_dir_path = os.path.dirname(self.model_trainer_config.trained_model_file_path)
-            os.makedirs(model_dir_path,exist_ok=True)
-            sensor_model = SensorModel(preprocessor=preprocessor,model=model)
+            os.makedirs(model_dir_path, exist_ok=True)
+            sensor_model = SensorModel(preprocessor=preprocessor, model=model)
             save_object(self.model_trainer_config.trained_model_file_path, obj=sensor_model)
 
-            #model trainer artifact
-
-            model_trainer_artifact = ModelTrainerArtifact(trained_model_file_path=self.model_trainer_config.trained_model_file_path, 
-            train_metric_artifact=classification_train_metric,
-            test_metric_artifact=classification_test_metric)
+            # Model trainer artifact
+            model_trainer_artifact = ModelTrainerArtifact(
+                trained_model_file_path=self.model_trainer_config.trained_model_file_path,
+                train_metric_artifact=classification_train_metric,
+                test_metric_artifact=classification_test_metric
+            )
             logging.info(f"Model trainer artifact: {model_trainer_artifact}")
             return model_trainer_artifact
         except Exception as e:
-            raise ApplicationException(e,sys)
+            raise ApplicationException(e, sys)
